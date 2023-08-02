7c2fe31ae89591dae0590a25940332db4a0ce1b8
Segaran <blsgrn@tutanota.com>
2023-08-02 11:55:08 +0530

    install and configure tailwindcss and create main container

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,8 @@
+<script>
+	import '../app.css';
+</script>
+
+<div class="container mx-auto my-6 max-w-lg">
+	<!-- container is a ulitility class, mx-auto to center horizontall and max width set to large -->
+	<slot />
+</div>
